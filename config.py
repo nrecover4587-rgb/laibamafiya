34f6148dc8d2935de4230eebf5e1b58928953da8
nrecover4587-rgb <nrecover4587@gmail.com>
2026-03-06 03:35:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", "YOUR_GIT_TOKEN")
 
 PRIVACY_LINK = getenv("PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-YukkiMusic-08-30")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/codexempire")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/mistubots")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/mentalchatting")
 
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "False")
